dea4d343a1a7e75efb3289d4934fdfaedc6a7ca6
Marc A. Donis <marc@balloonary.io>
2023-03-03 08:57:01 +0100

    .

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 let t = 0;
 let canvas, ctx;
-const mousePos = { x: 0, y: 0 };
+const mousePos = [];
 
 function load() {
-	document.addEventListener('mousemove', (e) => {
-		mousePos.x = e.clientX;
-		mousePos.y = e.clientY;
+	document.addEventListener('mouseup', (e) => {
+		mousePos.push({
+			x: e.clientX,
+			y: e.clientY,
+		});
 	});
 
 	canvas = document.getElementById('canvas');
@@ -19,22 +21,24 @@ function paint() {
 	ctx.fillStyle = '#a00';
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-	ctx.lineWidth = 8;
-	for (
-		let r = 0;
-		r < Math.max(canvas.width, canvas.height);
-		r += ctx.lineWidth
-	) {
-		const opacity = Math.round(127 * (Math.sin(t / 8 + r / 20) + 1));
-		let opacityStr = opacity.toString(16);
-		if (opacityStr.length < 2) {
-			opacityStr = `0${opacityStr}`;
+	ctx.lineWidth = 4;
+	for (const pos of mousePos) {
+		for (
+			let r = 0;
+			r < Math.max(canvas.width, canvas.height);
+			r += ctx.lineWidth
+		) {
+			const opacity = Math.round(127 * (Math.sin(t / 8 + r / 20) + 1));
+			let opacityStr = opacity.toString(16);
+			if (opacityStr.length < 2) {
+				opacityStr = `0${opacityStr}`;
+			}
+			ctx.strokeStyle = `#000000${opacityStr}`;
+			// console.log(r, opacityStr);
+			ctx.beginPath();
+			ctx.arc(pos.x, pos.y, r, 0, 2 * Math.PI);
+			ctx.stroke();
 		}
-		ctx.strokeStyle = `#000000${opacityStr}`;
-		// console.log(r, opacityStr);
-		ctx.beginPath();
-		ctx.arc(mousePos.x, mousePos.y, r, 0, 2 * Math.PI);
-		ctx.stroke();
 	}
 
 	t++;
